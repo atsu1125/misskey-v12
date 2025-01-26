7a81d3d027487a9147ced158aa0f94bf98240910
atsu1125 <atsuchan@atsuchan.page>
2025-01-26 21:31:06 +0900

    fix(backend): send out report abuse email up to 3 active moderators

--- packages/backend/src/queue/processors/db/report-abuse.ts
+++ packages/backend/src/queue/processors/db/report-abuse.ts
@@ -62,11 +62,18 @@ export async function reportAbuse(job: Bull.Job<DbAbuseReportJobData>): Promise<
 			}, {
 				isModerator: true,
 			}],
+			order: {
+				lastActiveDate: 'DESC',
+			},
 		});
 
 		const meta = await fetchMeta();
 
+		let emailSentCount = 0;
+
 		for (const moderator of moderators) {
+			if (emailSentCount >= 3) break;
+
 			publishAdminStream(moderator.id, 'newAbuseUserReport', {
 				id: job.data.id,
 				targetUserId: job.data.targetUserId,
@@ -83,6 +90,7 @@ export async function reportAbuse(job: Bull.Job<DbAbuseReportJobData>): Promise<
 					emailDeliver(emailRecipientProfile.email, 'New abuse report',
 						sanitizeHtml(job.data.comment),
 						sanitizeHtml(job.data.comment));
+					emailSentCount++;
 				}
 			}
 		}
