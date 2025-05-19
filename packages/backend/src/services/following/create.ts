d83f3b7a061bfcec6ce07c2e961c28588afd6406
atsu1125 <atsuchan@atsuchan.page>
2025-05-19 16:10:34 +0900

    fix(backend): avoid notifying to remote users on local
    Co-Authored-By: zyoshoka <zyoshoka@users.noreply.github.com>
    Co-Authored-By: かっこかり <kakkokari-gtyih@users.noreply.github.com>
    Co-Authored-By: syuilo <syuilo@users.noreply.github.com>

--- packages/backend/src/services/following/create.ts
+++ packages/backend/src/services/following/create.ts
@@ -59,9 +59,11 @@ export async function insertFollowingDoc(followee: { id: User['id']; host: User[
 		});
 
 		// 通知を作成
-		createNotification(follower.id, 'followRequestAccepted', {
-			notifierId: followee.id,
-		});
+		if (Users.isLocalUser(follower)) {
+			createNotification(follower.id, 'followRequestAccepted', {
+				notifierId: followee.id,
+			});
+		}
 	}
 
 	if (alreadyFollowed) return;
