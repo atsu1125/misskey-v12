1840095b3e7bbe97c05ef355fa67435e982fcf8d
atsu1125 <atsuchan@atsuchan.page>
2025-08-13 18:15:11 +0900

    Node 20.19.4
    drop Node v18
    Add test Node v20, v22, v24

--- packages/backend/src/server/web/manifest.ts
+++ packages/backend/src/server/web/manifest.ts
@@ -1,6 +1,6 @@
 import Koa from 'koa';
 import { fetchMeta } from '@/misc/fetch-meta.js';
-import manifest from './manifest.json' assert { type: 'json' };
+import manifest from './manifest.json' with { type: 'json' };
 
 export const manifestHandler = async (ctx: Koa.Context) => {
 	// TODO
