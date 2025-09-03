c93b2dd5f87f3b984d48e14dada43ca1842fefe7
atsu1125 <atsuchan@atsuchan.page>
2025-09-03 20:10:44 +0900

    enhance(backend): Use media proxy for emoji if enabled

--- packages/backend/src/models/repositories/emoji.ts
+++ packages/backend/src/models/repositories/emoji.ts
@@ -2,12 +2,28 @@ import { db } from '@/db/postgre.js';
 import { Emoji } from '@/models/entities/emoji.js';
 import { Packed } from '@/misc/schema.js';
 import { sanitizeUrl } from '@/misc/sanitize-url.js';
+import config from '@/config/index.js';
+import { query, appendQuery } from '@/prelude/url.js';
 
 export const EmojiRepository = db.getRepository(Emoji).extend({
 	async pack(
 		src: Emoji['id'] | Emoji,
 	): Promise<Packed<'Emoji'>> {
 		const emoji = typeof src === 'object' ? src : await this.findOneByOrFail({ id: src });
+
+		let emojiUrl = sanitizeUrl(emoji.publicUrl || emoji.originalUrl);
+
+		// リモートかつメディアプロキシ
+		if ((emoji.publicUrl != null || emoji.originalUrl != null) && emoji.host != null && config.mediaProxy != null) {
+			emojiUrl = appendQuery(config.mediaProxy, query({
+				url: sanitizeUrl(emoji.publicUrl || emoji.originalUrl)!,
+			}));
+		// リモートかつローカルプロキシ
+		} else if ((emoji.publicUrl != null || emoji.originalUrl != null) && emoji.host != null && config.proxyRemoteFiles) {
+			emojiUrl = `${config.url}/proxy/image.webp?${query({
+				url: sanitizeUrl(emoji.publicUrl || emoji.originalUrl)!,
+			})}`;
+		}
 
 		return {
 			id: emoji.id,
@@ -16,7 +32,7 @@ export const EmojiRepository = db.getRepository(Emoji).extend({
 			category: emoji.category,
 			host: emoji.host,
 			// || emoji.originalUrl してるのは後方互換性のため
-			url: sanitizeUrl(emoji.publicUrl || emoji.originalUrl)!,
+			url: sanitizeUrl(emojiUrl)!,
 		};
 	},
 
