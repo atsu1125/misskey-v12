cbc531480ea886476c493e48704a6448d146a586
atsu1125 <atsuchan@atsuchan.page>
2025-12-16 14:14:56 +0900

    fix(backend): reject follow if followee is moved

--- packages/backend/src/services/following/create.ts
+++ packages/backend/src/services/following/create.ts
@@ -168,6 +168,18 @@ export default async function(_follower: { id: User['id'] }, _followee: { id: Us
 		return;
 	}
 
+	if (followee.movedToUserId != null) {
+		if (Users.isRemoteUser(follower) && Users.isLocalUser(followee)) {
+			//　リモートフォローを受けて引っ越し済みなら、Rejectを送り返しておしまい。
+			const content = renderActivity(renderReject(renderFollow(follower, followee, requestId), followee));
+			deliver(followee , content, follower.inbox);
+			return;
+		} else {
+			//　フォロー対象が引っ越し済みなら、エラー。
+			throw new IdentifiableError('81f7e3eb-fbb8-313a-5603-bf2ce55644f9', 'moved');
+		}
+	}
+
 	const followeeProfile = await UserProfiles.findOneByOrFail({ userId: followee.id });
 
 	// フォロー対象が鍵アカウントである or
