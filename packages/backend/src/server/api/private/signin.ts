c8ad95a159a2db3667f9e15ced8f20b61269276f
atsu1125 <atsuchan@atsuchan.page>
2026-07-06 20:25:45 +0900

    Merge commit from fork
    fix(backend): Prevent the reuse of used TOTP tokens
    
    tighten totp window

--- packages/backend/src/server/api/private/signin.ts
+++ packages/backend/src/server/api/private/signin.ts
@@ -13,6 +13,7 @@ import { randomBytes } from 'node:crypto';
 import { IsNull } from 'typeorm';
 import { limiter } from '../limiter.js';
 import { getIpHash } from '@/misc/get-ip-hash.js';
+import { redisClient } from '../../../db/redis.js';
 
 export default async (ctx: Koa.Context) => {
 	ctx.set('Access-Control-Allow-Origin', config.url);
@@ -133,22 +134,65 @@ export default async (ctx: Koa.Context) => {
 			return;
 		}
 
-		const verified = (speakeasy as any).totp.verify({
+		// 判定に用いるタイムスタンプを固定
+		const now = Date.now();
+		const normalizedToken = token.trim();
+		const validationWindow = 1;
+		const timeStep = 30;
+
+		// 固定したタイムスタンプで検証
+		const delta = (speakeasy as any).totp.verifyDelta({
 			secret: profile.twoFactorSecret,
 			encoding: 'base32',
-			token: token,
-			window: 2,
+			token: normalizedToken,
+			step: timeStep,
+			window: validationWindow,
+			time: Math.floor(now / 1000),
 		});
 
-		if (verified) {
-			signin(ctx, user);
-			return;
-		} else {
+		if (!delta) {
 			await fail(403, {
 				id: 'cdf1235b-ac71-46d4-a3a6-84ccce48df6f',
 			});
 			return;
 		}
+
+		const currentStep = Math.floor(now / 1000 / timeStep);
+		const step = currentStep + delta.delta;
+
+		const usedTokenRedisKey =	`2fa:used:${user.id}:${step}`;
+
+		const ttl = timeStep * (validationWindow * 2 + 1);
+
+		await new Promise<void>((resolve, reject) => {
+			redisClient.set(
+				usedTokenRedisKey,
+				normalizedToken,
+				'EX',
+				ttl,
+				'NX', async (err, reply) => {
+						if (err) {
+								reject(err);
+								return;
+						}
+
+						try {
+								if (reply !== 'OK') {
+										await fail(403, {
+												id: 'cdf1235b-ac71-46d4-a3a6-84ccce48df6f',
+										});
+										resolve();
+										return;
+								}
+
+								signin(ctx, user);
+								resolve();
+						} catch (e) {
+								reject(e);
+						}
+			});
+		});
+		return;
 	} else if (body.credentialId) {
 		if (!same && !profile.usePasswordLessLogin) {
 			await fail(403, {
