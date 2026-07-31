0453c280390b5a360de4256a3c076ebde68d4481
atsu1125 <atsuchan@atsuchan.page>
2026-07-31 21:37:31 +0900

    fix(backend): define type of reactionsCount on stats API
    Co-Authored-By: かっこかり <kakkokari-gtyih@users.noreply.github.com>

--- packages/backend/src/server/api/endpoints/stats.ts
+++ packages/backend/src/server/api/endpoints/stats.ts
@@ -28,6 +28,14 @@ export const meta = {
 				type: 'number',
 				optional: false, nullable: false,
 			},
+			reactionsCount: {
+				type: 'number',
+				optional: false, nullable: false,
+			},
+			//originalReactionsCount: {
+			//	type: 'number',
+			//	optional: false, nullable: false,
+			//},
 			instances: {
 				type: 'number',
 				optional: false, nullable: false,
