c9e4e3b0bb1b3460b5a0e585ce630616f0356532
atsu1125 <atsuchan@atsuchan.page>
2026-08-01 15:02:49 +0900

    fix(backend): Handle inaccessible note on creating reaction

--- packages/backend/src/server/api/endpoints/notes/reactions/create.ts
+++ packages/backend/src/server/api/endpoints/notes/reactions/create.ts
@@ -56,6 +56,7 @@ export default define(meta, paramDef, async (ps, user) => {
 		if (e.id === '51c42bb4-931a-456b-bff7-e5a8a70dd298') throw new ApiError(meta.errors.alreadyReacted);
 		if (e.id === 'e70412a4-7197-4726-8e74-f3e0deb92aa7') throw new ApiError(meta.errors.youHaveBeenBlocked);
 		if (e.id === '12c35529-3c79-4327-b1cc-e2cf63a71925') throw new ApiError(meta.errors.cannotReactToRenote);
+		if (e.id === '68e9d2d1-48bf-42c2-b90a-b20e09fd3d48') throw new ApiError(meta.errors.noSuchNote);
 		throw e;
 	});
 	return;
