5514dfaf0c01fc2be1dd1fe23c03841c87789171
atsu1125 <atsuchan@atsuchan.page>
2026-09-14 20:56:30 +0900

    Merge commit from fork
    fix(backend): escapeHtml on email

--- packages/backend/src/services/send-email.ts
+++ packages/backend/src/services/send-email.ts
@@ -1,7 +1,9 @@
 import * as nodemailer from 'nodemailer';
+import sanitizeHtml from 'sanitize-html';
 import { fetchMeta } from '@/misc/fetch-meta.js';
 import Logger from './logger.js';
 import config from '@/config/index.js';
+import { escapeHtml } from '@/misc/escape-html.js';
 
 export const logger = new Logger('email');
 
@@ -14,6 +16,8 @@ export async function sendEmail(to: string, subject: string, html: string, text:
 	const emailSettingUrl = `${config.url}/settings/email`;
 
 	const enableAuth = meta.smtpUser != null && meta.smtpUser !== '';
+
+	const sanitizedHtml = sanitizeHtml(html);
 
 	// メールドメインブロックしてたら中断
 	const domain: string = to.split('@')[1];
@@ -36,7 +40,6 @@ export async function sendEmail(to: string, subject: string, html: string, text:
 	} as any);
 
 	try {
-		// TODO: htmlサニタイズ
 		const info = await transporter.sendMail({
 			from: meta.email!,
 			to: to,
@@ -46,7 +49,7 @@ export async function sendEmail(to: string, subject: string, html: string, text:
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>${ subject }</title>
+		<title>${ escapeHtml(subject) }</title>
 		<style>
 			html {
 				background: #eee;
@@ -107,18 +110,18 @@ export async function sendEmail(to: string, subject: string, html: string, text:
 	<body>
 		<main>
 			<header>
-				<img src="${ meta.logoImageUrl || meta.iconUrl || iconUrl }"/>
+				<img src="${ escapeHtml(meta.logoImageUrl || meta.iconUrl || iconUrl) }"/>
 			</header>
 			<article>
-				<h1>${ subject }</h1>
-				<div>${ html }</div>
+				<h1>${ escapeHtml(subject) }</h1>
+				<div>${ sanitizedHtml }</div>
 			</article>
 			<footer>
-				<a href="${ emailSettingUrl }">${ 'Email setting' }</a>
+				<a href="${ escapeHtml(emailSettingUrl) }">${ 'Email setting' }</a>
 			</footer>
 		</main>
 		<nav>
-			<a href="${ config.url }">${ config.host }</a>
+			<a href="${ escapeHtml(config.url) }">${ escapeHtml(config.host) }</a>
 		</nav>
 	</body>
 </html>`,
